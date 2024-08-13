2e7c517b04059e4391b247054d814697ab6e5df1
AakashSankarAS <aakashsankar412@gmail.com>
2024-08-13 13:52:16 +0530

    new

--- src/pages/meeting/EmbeddedMeeting.tsx
+++ src/pages/meeting/EmbeddedMeeting.tsx
@@ -2,7 +2,7 @@ import { useLocation } from "react-router-dom";
 
 export default function EmbeddedMeeting() {
   const { state } = useLocation();
-  const proxyUrl = `https://onlineappointment.onrender.com/proxy/meeting?meetingKey=${state?.meetingKey}&encryptPwd=${state?.encryptPwd}`;
+  const proxyUrl = `https://meeting.zoho.in/meeting/login/embedmeeting.jsp?meetingKey=${state?.meetingKey}&t=${state?.encryptPwd}`;
 
   return <iframe src={proxyUrl} width="100%" height="600px" frameBorder="0" />;
 }
